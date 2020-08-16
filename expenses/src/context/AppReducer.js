a8016db954d1e5e1677d0f7f3b2de44c1195a0b5
Antonio Daria <antoniodaria@msn.com>
2020-08-16 18:27:47 +0100

    added app reducer

---
+++ expenses/src/context/AppReducer.js
@@ -0,0 +1,8 @@
+// We need a Reducer to specify the application state changes to our store(context) in response to certain actions
+
+export default (state, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
