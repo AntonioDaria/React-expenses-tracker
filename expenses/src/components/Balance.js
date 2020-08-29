2d0bf0af8ee5aaae8a65b36e926b354ee92d9ff5
Antonio Daria <antoniodaria@msn.com>
2020-08-29 20:12:45 +0100

    Update balance and transaction component

--- expenses/src/components/Balance.js
+++ expenses/src/components/Balance.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const Balance = () => {
-    return (
-        <>
-          <h4>Your Balance</h4>
-          <h1>$0.00</h1>  
-        </>
-    )
+  const { transactions } = useContext(GlobalContext)
+
+
+  const arrayBalance = transactions.map(transaction => { return transaction.amount })
+  const total = arrayBalance.reduce((acc, item) => {
+    return (acc += item)
+  }, 0).toFixed(2);
+
+  return (
+    <>
+      <h4>Your Balance</h4>
+      <h1>${total}</h1>
+    </>
+  )
 }
 
 export default Balance
--- expenses/src/components/Transaction.spec.js
+++ expenses/src/components/Transaction.spec.js
@@ -2,20 +2,21 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Transaction from './Transaction.js'
 
-const MOCK_TRANSACTIONS = [
+const MOCK_TRANSACTIONS = 
     [
         { id: 1, text: 'Flower', amount: -20 },
         { id: 2, text: 'Salary', amount: 300 },
         { id: 3, text: 'Book', amount: -10 },
         { id: 4, text: 'Camera', amount: 150 }
     ]
-]
+
 
 describe('Transaction component', () => {
     let container;
 
+
     beforeEach(() => {
-        ({ container } = renderTransaction());
+        ({ container} = renderTransaction());
     });
 
     it('should match the snapshot', () => {
