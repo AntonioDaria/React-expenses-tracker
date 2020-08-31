7a0554677137153ff11502fddf54de5f3ac62f1d
Antonio Daria <antoniodaria@msn.com>
2020-08-31 11:42:15 +0100

    Add additional snapshot test for Balance.spec

--- expenses/src/components/Balance.spec.js
+++ expenses/src/components/Balance.spec.js
@@ -1,23 +1,45 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, wait } from '@testing-library/react';
 import Balance from './Balance.js'
+import { GlobalContext, INITIAL_TRANSACTION_CONTEXT_STATE} from '../context/GlobalState'
 
+
+let dispatch = jest.fn();
 describe('Balance component', () => {
     let container;
 
-    beforeEach(() => {
-      ({ container} = renderBalance());
+    it('should match the snapshot', async () => {
+      const renderBalance = () => {
+        return render(
+            <Balance/>
+        );
+      };
+      ({ container } = renderBalance());
+      expect(container).toMatchSnapshot();
+    });
+    
+
+    it('should display the right balance', async () => {
+      ({ container } = renderBalanceWithContext());
+      await wait();
+      expect(container).toMatchSnapshot();
     });
 
-    it('should match the snapshot', () => {
-        expect(container).toMatchSnapshot();
-    });
+     
 });
+  
 
-const renderBalance = () => {
-    return render(
-        <Balance/>
-    );
-  };
-  
-  
+const renderBalanceWithContext = () => {
+  let mockTransactions = INITIAL_TRANSACTION_CONTEXT_STATE.transactions
+  return render(
+    
+    <GlobalContext.Provider
+    value={{
+      dispatch,
+      transactions: mockTransactions
+    }}
+    >
+       <Balance/>
+    </GlobalContext.Provider>
+  );
+};
